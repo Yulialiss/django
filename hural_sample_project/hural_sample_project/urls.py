dd4c0c9f3f8ddcc2395eca07b9c5496183a7e392
Юлія <yuliahuralgit@gmail.com>
2024-10-11 10:06:09 +0000

    fixed problem

--- hural_sample_project/hural_sample_project/urls.py
+++ hural_sample_project/hural_sample_project/urls.py
@@ -6,10 +6,5 @@ urlpatterns = [
     path('calculator/', include('calculator_app.urls')),
     path('guess/', include('guess_game_app.urls')),
     path('hello/', include('hello_app.urls')),
-<<<<<<< HEAD
     path('', include('home_app.urls')),
 ]
-=======
-    path('', include('home_app.urls')), 
-]
->>>>>>> beb7fca584c04289a027045a2abc377d36129d85
